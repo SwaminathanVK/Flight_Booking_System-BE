cf3baf8e06dbe52f456fd4c7f64bf9c61fe89c23
SwaminathanVK <swami19@gmail.com>
2025-08-11 11:55:28 +0530

    updated the models

--- models/userSchema.js
+++ models/userSchema.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 
   role: {
     type: String,
